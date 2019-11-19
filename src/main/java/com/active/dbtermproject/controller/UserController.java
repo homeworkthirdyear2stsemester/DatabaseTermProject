4ed8c6db5e1a917267e0bad3f3898a834f2fcf33
kihwanKim <clack2933@gmail.com>
2019-11-19 17:42:37 +0900

    connect the jsp page and back end codes

--- src/main/java/com/active/dbtermproject/controller/UserController.java
+++ src/main/java/com/active/dbtermproject/controller/UserController.java
@@ -3,12 +3,17 @@ package com.active.dbtermproject.controller;
 import com.active.dbtermproject.domain.User;
 import com.active.dbtermproject.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
-@RequestMapping(path = "user")
+@Controller
+@RequestMapping("/user")
 public class UserController { // front와 backend 연결 다리 역할
 
     @Autowired
@@ -27,5 +32,12 @@ public class UserController { // front와 backend 연결 다리 역할
     @PostMapping(path = "/insert")
     public int insertUser(@RequestBody User user) {
         return this.userService.insertService(user);
+    }
+
+    @GetMapping("/test") // localhost/user/test
+    public String test(Model model) {
+        model.addAttribute("user", new User());
+
+        return "test";       // 실제 호출될페이지 : /WEB-INF/jsp/test.jsp
     }
 }
